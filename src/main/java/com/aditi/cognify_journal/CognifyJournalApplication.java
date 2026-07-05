2cc10538225e5036367a46caafea23bde7a22586
Aditi <adi515choudhary@gmail.com>
2026-07-05 19:39:57 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/aditi/cognify_journal/CognifyJournalApplication.java
@@ -0,0 +1,13 @@
+package com.aditi.cognify_journal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CognifyJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CognifyJournalApplication.class, args);
+	}
+
+}
